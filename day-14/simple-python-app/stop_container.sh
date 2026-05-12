23ee28105544877c5d8c8cabce1466b272625b07
Harishmoppor <114092994+Harishmoppor@users.noreply.github.com>
2026-05-12 14:59:02 +0530

    Modify stop_container.sh to handle container removal
    Updated script to stop and remove the running container.

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop and remove the container if it's running
+if docker ps -q -f name=flask-app | grep -q .; then
+  docker stop flask-app
+  docker rm flask-app
+fi
